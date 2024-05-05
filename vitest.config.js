41e9480d25328b41f2680d5ffbf6b19108e58b95
fmpberger88 <fabian.berger@protonmail.ch>
2024-05-05 13:16:17 +0200

    setup test environment

---
+++ vitest.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    rootDir: '__tests__',
+    testMatch: '**/*.test.jsx',
+    plugins: [react()],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './__tests__/setup.js',
+    }
+})
