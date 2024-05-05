5eaae174922d4d57374b2f2ed61d6cf0b42b5255
fmpberger88 <fabian.berger@protonmail.ch>
2024-05-05 18:06:45 +0200

    setup AddressComponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,56 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import './App.css'
+import AddressComponent from "./components/AddressComponent/AddressComponent.jsx";
 
 function App() {
-    const [address, setAddress] = useState({
-        firstname: '',
-        lastname: '',
-        street: '',
-        zipcode: '',
-        city: '',
-        country: '',
+    // Default Address
+    const defaultAddress = {
+        firstName: 'Fabian Marc Pascal',
+        lastName: 'Berger',
+        street: 'Oerlifallstieg 5',
+        city: 'Schaffhausen',
+        zipcode: '8200',
+        country: 'Switzerland',
+        birthdate: '1988-08-19'
+    };
 
-    })
+    // Default Address or from LocalStorage
+    const [address, setAddress] = useState(() => {
+        const savedAddress = localStorage.getItem('address');
+        return savedAddress ? JSON.parse(savedAddress) : defaultAddress;
+    });
+    const [showDefaults, setShowDefaults] = useState(true);
+
+    // useEffect for save address to localStorage
+    useEffect(() => {
+        localStorage.setItem('address', JSON.stringify(address));
+    }, [address]);
+
+    // Functions
+    const hideDefaults = () => {
+        setAddress({
+            firstName: '',
+            lastName: '',
+            street: '',
+            city: '',
+            zipcode: '',
+            country: '',
+            birthdate: ''
+        });
+        setShowDefaults(false);
+    }
+
   return (
     <>
-        <h1>Our First Test</h1>
+        <div>
+            <AddressComponent address={address} onAddressChange={setAddress} />
+            {showDefaults && (
+                <button onClick={hideDefaults}>Hide Defaults</button>
+            )}
+        </div>
+        <div>
+            {/* Preview */}
+        </div>
     </>
   )
 }
--- src/components/AddressComponent/AddressComponent.jsx
+++ src/components/AddressComponent/AddressComponent.jsx
@@ -1,5 +1,66 @@
 function AddressComponent({ address, onAddressChange }) {
+    // Handle input field changes
+    const handleChange = (event) => {
+        const { name, value} = event.target;
+        onAddressChange({
+            ...address,
+            [name]: value
+        });
+    };
 
+    return (
+        <form>
+            <input
+                type="text"
+                name="firstName"
+                value={address.firstName || ''}
+                onChange={handleChange}
+                placeholder="First Name"
+            />
+            <input
+                type="text"
+                name="lastName"
+                value={address.lastName || ''}
+                onChange={handleChange}
+                placeholder="Last Name"
+            />
+            <input
+                type="text"
+                name="street"
+                value={address.street}
+                onChange={handleChange}
+                placeholder="Street Name"
+            />
+            <input
+                type="text"
+                name="city"
+                value={address.city}
+                onChange={handleChange}
+                placeholder="City"
+            />
+            <input
+                type="text"
+                name="zipcode"
+                value={address.zipcode}
+                onChange={handleChange}
+                placeholder="Zipcode"
+            />
+            <input
+                type="text"
+                name="country"
+                value={address.country}
+                onChange={handleChange}
+                placeholder="Country"
+            />
+            <input
+                type="date"
+                name="birthdate"
+                value={address.birthdate}
+                onChange={handleChange}
+                placeholder="Birthdate"
+            />
+        </form>
+    )
 }
 
 export default AddressComponent;
