35f19ac702e7c39382853d958e0f35968d329662
fmpberger88 <fabian.berger@protonmail.ch>
2024-05-13 21:27:56 +0200

    validate inputs for EducationComponent

--- src/components/EducationComponent/EducationComponent.jsx
+++ src/components/EducationComponent/EducationComponent.jsx
@@ -1,7 +1,10 @@
-import {AdressForm, EducationContainer, EducationContainerTitle, InputContainers} from "../../styles.jsx";
-import { FaPlus, FaMinus } from 'react-icons/fa'
+import React, { useState } from 'react';
+import { AdressForm, EducationContainer, EducationContainerTitle, InputContainers } from "../../styles.jsx";
+import { FaPlus, FaMinus } from 'react-icons/fa';
 
 const EducationComponent = ({ educations, setEducations }) => {
+    const [errors, setErrors] = useState({});
+
     const addEducation = () => {
         const newEducation = {
             id: Date.now(),
@@ -21,23 +24,64 @@ const EducationComponent = ({ educations, setEducations }) => {
 
     const handleInputChange = (id, field, value) => {
         setEducations(educations.map(education =>
-            education.id === id ? {...education, [field]: value} : education
+            education.id === id ? { ...education, [field]: value } : education
         ));
+        validateInput(id, field, value);
+    };
+
+    const validateInput = (id, field, value) => {
+        let error = '';
+        switch (field) {
+            case 'school':
+                if (!value) {
+                    error = 'School/University cannot be empty';
+                }
+                break;
+            case 'degree':
+                if (!value) {
+                    error = 'Degree cannot be empty';
+                }
+                break;
+            case 'fieldOfStudy':
+                if (!value) {
+                    error = 'Field of study cannot be empty';
+                }
+                break;
+            case 'startYear':
+                if (!/^(0[1-9]|1[0-2])\/\d{4}$/.test(value)) {
+                    error = 'Start year must be in MM/YYYY format';
+                }
+                break;
+            case 'endYear':
+                if (value && !/^(0[1-9]|1[0-2])\/\d{4}$/.test(value)) {
+                    error = 'End year must be in MM/YYYY format';
+                }
+                break;
+            default:
+                break;
+        }
+        setErrors({ ...errors, [`${id}-${field}`]: error });
     };
 
     const toggleExpand = (id) => {
         setEducations(educations.map(education =>
-        education.id === id ? {...education, isExpanded: !education.isExpanded} : education
-        ))
-    }
+            education.id === id ? { ...education, isExpanded: !education.isExpanded } : education
+        ));
+    };
+
+    const hasErrors = (id) => {
+        return Object.keys(errors).some(key => key.startsWith(id) && errors[key]);
+    };
 
     return (
         <InputContainers>
             {educations && educations.map((education) => (
                 <AdressForm key={education.id}>
                     <EducationContainer>
-                        <EducationContainerTitle onClick={() => toggleExpand(education.id)}>{education.school}</EducationContainerTitle>
-                        {education.isExpanded ? <FaMinus onClick={() => toggleExpand(education.id)}/> : <FaPlus onClick={() => toggleExpand(education.id)}/>}
+                        <EducationContainerTitle onClick={() => toggleExpand(education.id)}>
+                            {education.school || 'New Education'}
+                        </EducationContainerTitle>
+                        {education.isExpanded ? <FaMinus onClick={() => toggleExpand(education.id)} /> : <FaPlus onClick={() => toggleExpand(education.id)} />}
                     </EducationContainer>
                     {education.isExpanded && (
                         <>
@@ -47,33 +91,38 @@ const EducationComponent = ({ educations, setEducations }) => {
                                 onChange={e => handleInputChange(education.id, 'school', e.target.value)}
                                 placeholder="School/University"
                             />
+                            {errors[`${education.id}-school`] && <span>{errors[`${education.id}-school`]}</span>}
                             <input
                                 type="text"
                                 value={education.degree}
                                 onChange={e => handleInputChange(education.id, 'degree', e.target.value)}
                                 placeholder="Degree"
                             />
+                            {errors[`${education.id}-degree`] && <span>{errors[`${education.id}-degree`]}</span>}
                             <input
                                 type="text"
                                 value={education.fieldOfStudy}
                                 onChange={e => handleInputChange(education.id, 'fieldOfStudy', e.target.value)}
                                 placeholder="Subject"
                             />
+                            {errors[`${education.id}-fieldOfStudy`] && <span>{errors[`${education.id}-fieldOfStudy`]}</span>}
                             <input
                                 type="text"
                                 value={education.startYear}
                                 onChange={e => handleInputChange(education.id, 'startYear', e.target.value)}
                                 placeholder="from: MM/YYYY"
                             />
+                            {errors[`${education.id}-startYear`] && <span>{errors[`${education.id}-startYear`]}</span>}
                             <input
                                 type="text"
                                 value={education.endYear}
                                 onChange={e => handleInputChange(education.id, 'endYear', e.target.value)}
                                 placeholder="to: MM/YYYY"
                             />
+                            {errors[`${education.id}-endYear`] && <span>{errors[`${education.id}-endYear`]}</span>}
                             <button onClick={(e) => {
                                 e.stopPropagation();
-                                removeEducation(education.id)
+                                removeEducation(education.id);
                             }}>Remove</button>
                         </>
                     )}
